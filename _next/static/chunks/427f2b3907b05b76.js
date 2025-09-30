807a992e81e525ca9937299d61c86027fcde5b9f
ChamaraSapumal <chamarasecu.21@gmail.com>
2025-09-30 21:03:41 +0530

    Updates

---
+++ _next/static/chunks/427f2b3907b05b76.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/2892b7d09d0a417d.js",
+  "static/chunks/turbopack-8c28661cc0afdb8a.js"
+])
---
+++ _next/static/chunks/b857a3a82a56a47c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/2892b7d09d0a417d.js",
+  "static/chunks/turbopack-62f602aa32e78deb.js"
+])
